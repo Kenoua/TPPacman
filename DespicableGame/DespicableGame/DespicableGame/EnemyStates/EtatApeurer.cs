6575665f0a7cc1587963a4e967875017c0d4739b
Sébastien Rousseau <sebrousseau33@gmail.com>
2015-09-14 08:36:38 -0400

    Apeurer

--- DespicableGame/DespicableGame/DespicableGame/EnemyStates/EtatApeurer.cs
+++ DespicableGame/DespicableGame/DespicableGame/EnemyStates/EtatApeurer.cs
@@ -30,20 +30,39 @@ namespace DespicableGame.EnemyStates
             personnage.VitesseX = 0;
             personnage.VitesseY = 0;
 
+            if (personnage.ActualCase.OrdreX == objectif.X && personnage.ActualCase.OrdreY == objectif.Y)
+                return personnage.ActualCase;
 
-            if (personnage.ActualCase.OrdreX != objectif.X)
+            List<int> directionsPriorises = new List<int>();
+
+            if (personnage.ActualCase.OrdreY < objectif.Y)
             {
-                bool culDeSac = false;
-                List<int> optionsInvalides = new List<int>();
+                directionsPriorises.Add(0);
+                directionsPriorises.Add(1);
+            }
+            else
+            {
+                directionsPriorises.Add(1);
+                directionsPriorises.Add(0);
+            }
+            if (personnage.ActualCase.OrdreX < objectif.X)
+            {
+                directionsPriorises.Add(2);
+                directionsPriorises.Add(3);
+            }
+            else
+            {
+                directionsPriorises.Add(3);
+                directionsPriorises.Add(2);
+            }
 
-                while (true)
+            foreach (int direction in directionsPriorises)
+            {
+                if (direction != personnage.DirectionArriere)
                 {
-                    int choixRandom = GenerateurChiffreAleatoire.NouveauChiffre(4);
-
-                    if (choixRandom != personnage.DirectionArriere || culDeSac)
+                    switch (direction)
                     {
-                        if (choixRandom == 0)
-                        {
+                        case 0:
                             if (!(AI_Case.CaseHaut == null || AI_Case.CaseHaut is Teleporteur))
                             {
                                 personnage.DirectionArriere = 2;
@@ -51,15 +70,8 @@ namespace DespicableGame.EnemyStates
                                 personnage.VitesseY = -4/*-DespicableGame.VITESSE*/;
                                 return AI_Case.CaseHaut;
                             }
-                            else
-                            {
-                                if (!optionsInvalides.Contains(choixRandom))
-                                    optionsInvalides.Add(choixRandom);
-                            }
-                        }
-
-                        if (choixRandom == 2)
-                        {
+                            break;
+                        case 1:
                             if (!(AI_Case.CaseBas == null || AI_Case.CaseBas is Teleporteur))
                             {
                                 personnage.DirectionArriere = 0;
@@ -67,31 +79,8 @@ namespace DespicableGame.EnemyStates
                                 personnage.VitesseY = 4;
                                 return AI_Case.CaseBas;
                             }
-                            else
-                            {
-                                if (!optionsInvalides.Contains(choixRandom))
-                                    optionsInvalides.Add(choixRandom);
-                            }
-                        }
-
-                        if (choixRandom == 1)
-                        {
-                            if (!(AI_Case.CaseGauche == null || AI_Case.CaseGauche is Teleporteur))
-                            {
-                                personnage.DirectionArriere = 3;
-                                personnage.VitesseX = -4;
-                                personnage.VitesseY = 0;
-                                return AI_Case.CaseGauche;
-                            }
-                            else
-                            {
-                                if (!optionsInvalides.Contains(choixRandom))
-                                    optionsInvalides.Add(choixRandom);
-                            }
-                        }
-
-                        if (choixRandom == 3)
-                        {
+                            break;
+                        case 2:
                             if (!(AI_Case.CaseDroite == null || AI_Case.CaseDroite is Teleporteur))
                             {
                                 personnage.DirectionArriere = 1;
@@ -99,21 +88,97 @@ namespace DespicableGame.EnemyStates
                                 personnage.VitesseY = 0;
                                 return AI_Case.CaseDroite;
                             }
-                            else
+                            break;
+                        case 3:
+                            if (!(AI_Case.CaseGauche == null || AI_Case.CaseGauche is Teleporteur))
                             {
-                                if (!optionsInvalides.Contains(choixRandom))
-                                    optionsInvalides.Add(choixRandom);
+                                personnage.DirectionArriere = 3;
+                                personnage.VitesseX = -4;
+                                personnage.VitesseY = 0;
+                                return AI_Case.CaseGauche;
                             }
-                        }
-
-                        if (optionsInvalides.Count() == 3)
-                        {
-
-                            culDeSac = true;
-                        }
+                            break;
+                        default:
+                            return personnage.Destination;
                     }
                 }
             }
+            return null;
+            //bool culDeSac = false;
+            //List<int> optionsInvalides = new List<int>();
+
+            //while (true)
+            //{
+            //    if (!culDeSac)
+            //    {
+            //        if (personnage.ActualCase.OrdreY < objectif.Y)
+            //        {
+            //            if (!(AI_Case.CaseHaut == null || AI_Case.CaseHaut is Teleporteur))
+            //            {
+            //                personnage.DirectionArriere = 2;
+            //                personnage.VitesseX = 0;
+            //                personnage.VitesseY = -4/*-DespicableGame.VITESSE*/;
+            //                return AI_Case.CaseHaut;
+            //            }
+            //            else
+            //            {
+            //                if (!optionsInvalides.Contains(0))
+            //                    optionsInvalides.Add(0);
+            //            }
+            //        }
+            //        else if (personnage.ActualCase.OrdreY > objectif.Y)
+            //        {
+            //if (!(AI_Case.CaseBas == null || AI_Case.CaseBas is Teleporteur))
+            //{
+            //    personnage.DirectionArriere = 0;
+            //    personnage.VitesseX = 0;
+            //    personnage.VitesseY = 4;
+            //    return AI_Case.CaseBas;
+            //}
+            //            else
+            //            {
+            //                if (!optionsInvalides.Contains(0))
+            //                    optionsInvalides.Add(0);
+            //            }
+            //        }
+            //        else if (personnage.ActualCase.OrdreX < objectif.X)
+            //        {
+            //if (!(AI_Case.CaseGauche == null || AI_Case.CaseGauche is Teleporteur))
+            //{
+            //    personnage.DirectionArriere = 3;
+            //    personnage.VitesseX = -4;
+            //    personnage.VitesseY = 0;
+            //    return AI_Case.CaseGauche;
+            //}
+            //            else
+            //            {
+            //                if (!optionsInvalides.Contains(2))
+            //                    optionsInvalides.Add(2);
+            //            }
+            //        }
+            //        else
+            //        {
+            //if (!(AI_Case.CaseDroite == null || AI_Case.CaseDroite is Teleporteur))
+            //{
+            //    personnage.DirectionArriere = 1;
+            //    personnage.VitesseX = 4;
+            //    personnage.VitesseY = 0;
+            //    return AI_Case.CaseDroite;
+            //}
+            //            else
+            //            {
+            //                if (!optionsInvalides.Contains(3))
+            //                    optionsInvalides.Add(3);
+            //            }
+            //        }
+
+            //        if (optionsInvalides.Count() == 3)
+            //        {
+
+            //            culDeSac = true;
+            //        }
+            //    }
+
         }
     }
 }
