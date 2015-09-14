453b6743884688cb28cb572886127c0515e3aed7
Vincent Montminy <vingt_cent_20@hotmail.com>
2015-09-14 15:45:08 -0400

    Enlever shits du merge

--- DespicableGame/DespicableGame/DespicableGame/GameStates/EtatJeu.cs
+++ DespicableGame/DespicableGame/DespicableGame/GameStates/EtatJeu.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,28 +21,3 @@ namespace DespicableGame.GameStates
         bool HasExited();
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Content;
-
-namespace DespicableGame.GameStates
-{
-    public interface EtatJeu
-    {
-        void LoadContent(ContentManager _content);
-
-        void Update();
-
-        void HandleInput();
-
-        void Draw(SpriteBatch _spriteBatch);
-
-        bool HasExited();
-    }
-}
->>>>>>> master
