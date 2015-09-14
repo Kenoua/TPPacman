e8d402ddf35708c1062aa22396cc64d9cde95aed
Vincent Montminy <vingt_cent_20@hotmail.com>
2015-09-14 16:03:52 -0400

    Squelette Snorlax

--- DespicableGame/DespicableGame/DespicableGame/Snorlax.cs
+++ DespicableGame/DespicableGame/DespicableGame/Snorlax.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
 
 namespace DespicableGame
 {
-    class Snorlax
+    class Snorlax : Personnage
     {
+
+        public Snorlax(Texture2D dessin, Vector2 position, Case ActualCase)
+            : base(dessin, position, ActualCase)
+        {
+
+
+        }
     }
 }
